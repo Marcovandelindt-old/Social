7164269b70d054751805d52b4ad06a520986fb4a
Marco van de Lindt <marcovandelindt@live.nl>
2018-05-01 14:47:51 +0200

    Added home and css

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,12 +4,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller 
 {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+	}
+
 	/**
 	 * Load the home view
 	 */
-	public function index ()
+	public function index ($sPageName = 'home')
 	{
-		$data['page_name'] = 'home'; 
+		$data['pageName'] = 'home';
+
+		$this->load->view('partials/header', $data);
 		$this->load->view('home', $data);
+		$this->load->view('partials/footer', $data);
 	}
 }
---
+++ application/views/partials/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<!-- Meta Tags -->
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit-no" />
+
+		<title><?= ucfirst($pageName) ?></title>
+
+		<!-- Bootstrap CSS CDN -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+		<!-- Application CSS -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url() ?>/public/css/app.css" media="screen" />
+
+	</head>
+	<body>
