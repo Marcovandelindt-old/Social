06f026961c49c9241552e6e5c3657129076eca89
Marco van de Lindt <marcovandelindt@live.nl>
2018-05-02 16:33:35 +0200

    Main layout for home

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,27 @@
-Dit is de home view
+<br />
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="profile-block">
+
+			</div>
+		</div>
+		<div class="col-md-7">
+			<div class="status-block">
+
+			</div>
+			<div class="status-overview-block">
+
+			</div>
+		</div>
+		<div class="col-md-2">
+			<div class="online-friends-block">
+
+			</div>
+			<div class="friend-requests-block">
+				
+			</div>
+		</div>
+	</div>
+</div>
--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -17,3 +17,34 @@
 
 	</head>
 	<body>
+
+		<!-- Navigation -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light" id="mainNavigation">
+			<a class="navbar-brand" href="<?= base_url() ?>">
+				Social
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLinkList" aria-controls="navbarLinkList" aria-expaned="false">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarLinkList">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item <?= ( $pageName == 'home' ? 'active' : '' ) ?>">
+						<a class="nav-link" href="<?= base_url() ?>">Home</a>
+					</li>
+					<li class="nav-item <?= ( $pageName == 'mentions' ? 'active' : '' ) ?>">
+						<a class="nav-link" href="<?= base_url() ?>/mentions">Mentions</a>
+					</li>
+					<li class="nav-item <?= ( $pageName == 'messages' ? 'active' : '' ) ?>">
+						<a class="nav-link" href="<?= base_url() ?>/messages">Messages</a>
+					</li>
+					<li class="nav-item <?= ( $pageName == 'friends' ? 'active' : '' ) ?>">
+						<a class="nav-link" href="<?= base_url() ?>/friends">Friends</a>
+					</li>
+					<li class="nav-item <?= ( $pageName == 'profile' ? 'active' : '' ) ?>">
+						<a class="nav-link" href="<?= base_url() ?>/profile">My Profile</a>
+					</li>
+				</ul>
+			</div>
+		</nav> 
+		<!-- /Navigation -->
